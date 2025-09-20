1717780a9eff54c6c9ca368b8f5a70df43486f75
Hemal Shingloo <shingloo55@gmail.com>
2025-09-20 21:53:27 +0530

    Initial commit

---
+++ frnt-end/frnt-end-new/frnt-end/src/pages/auth.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AuthPage = () => {
+  return (
+    <div>
+      <h1>Authentication</h1>
+      <button>Sign in with Google</button>
+      <button>Sign up with Email</button>
+    </div>
+  );
+};
+
+export default AuthPage;
---
+++ frnt-end/frnt-end-new/frnt-end/src/pages/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>Welcome to VasatIQ</h1>
+      <nav>
+        <ul>
+          <li><a href="/about">About Us</a></li>
+          <li><a href="/services">Our Services</a></li>
+          <li><a href="/offer">What We Offer</a></li>
+          <li><a href="/contact">Contact Us</a></li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default LandingPage;
